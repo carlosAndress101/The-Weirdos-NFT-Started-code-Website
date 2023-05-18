770acf4bd9720a2d1ebec41eb793fd8f268544a4
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-05-18 01:28:15 -0500

    new section Team and added also faq

---
+++ src/components/Confetti.jsx
@@ -0,0 +1,17 @@
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
+function ConfettiComponent() {
+    const { width, height } = useWindowSize()
+    return (
+      <Confetti
+      numberOfPieces={150}
+      gravity={0.01}
+        width={width}
+        height={height*1.5}
+      />
+    )
+}
+
+export default ConfettiComponent
+
--- src/components/sections/Faq.jsx
+++ src/components/sections/Faq.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import styled from 'styled-components';
 
+const Section = styled.section`
+min-height: 100vh;
+width:100vw;
+background-color: ${props => props.theme.text};
+position: relative;
+color: ${props => props.theme.body};
+
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`;
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: uppercase;
+  color: ${props => props.theme.body};
+
+  margin:  1rem auto;
+  border-bottom: 2px solid ${props => props.theme.body};
+  width: fit-content;
+
+`;
+const Container = styled.div`
+width: 75%;
+margin: 2rem auto;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
+const Box = styled.div`
+width: 45%;
+`
 function Faq() {
   return (
-    <div>Faq</div>
+    <Section>
+    <Title>FAQ</Title>
+    <Container>
+    <Box>col1</Box>
+    <Box>col2</Box>
+    </Container>
+    </Section>
   )
 }
 
