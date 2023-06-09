5b5c98be7af9cbffe5a923e902bba588e3b2b4fa
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-06-09 01:24:55 -0700

    firt responsive

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Showcase from './components/sections/Showcase'
 import Team from './components/sections/Team'
 import Faq from './components/sections/Faq'
 import Footer from './components/Footer'
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
@@ -24,7 +25,8 @@ function App() {
         <Team/>        
         <Faq/>        
         <Footer/>        
-
+        <ScrollToTop/>
+        
       </ThemeProvider>
     </>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import Button from "./Button";
+import { useState } from "react";
 
 const Section = styled.section`
   width: 100vw;
@@ -20,7 +21,28 @@ display: flex;
 justify-content: space-between;
 align-items: center;
 list-style: none;
-`
+
+@media (max-width: 64em){
+/**1024 */
+position: fixed;
+top: ${props => props.theme.navHeight};
+left: 0;
+right: 0;
+bottom: 0;
+width: 100vw;
+height: ${props => `calc(100vh - ${props.theme.navHeight})`};
+z-index: 50;
+background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.85)`};
+backdrop-filter: blur(2px);
+
+transform: ${props => props.click ? 'translateY(0)' : `translateY(100%)`};
+transition: all 0.3s ease;
+
+flex-direction: column;
+justify-content: center;
+}
+`;
+
 const MenuItem = styled.li`
 margin: 0 1rem;
 color: ${props => props.theme.text};
@@ -39,11 +61,54 @@ cursor: pointer;
 &:hover::after{
   width: 100%;
 }
+`;
 
-`
+const HamburgerMenu  = styled.span`
+width: ${props => props.click ? '2rem' : '1.5rem'};
+height: 2px;
+background: ${props => props.theme.text};
+
+position: absolute;
+top: 2rem;
+left: 50%;
+transform: ${props => props.click ? 'translateX(-50%) rotate(90deg)' : 'translateX(-50%) rotate(0)'};
+
+display:none;
+justify-content: center;
+align-items: center;
+
+cursor: pointer;
+transition: all 0.3s ease;
+
+@media (max-width: 64em){
+/**1024 */
+display: flex;
+}
+
+&::after, &::before{
+  content: ' ';
+  width: ${props => props.click ? '1rem' : '1.5rem'};
+  height: 2px;
+  right: ${props => props.click ? '-2px' : '0'};
+  background: ${props => props.theme.text};
+  position: absolute;
+  transition: all 0.3s ease;
+}
+
+&::after{
+  top: ${props => props.click ? '0.3rem' : '0.5rem'};
+  transform: ${props => props.click ? 'rotate(-40deg)' : 'rotate(0)'};
+}
+&::before{
+  bottom: ${props => props.click ? '0.3rem' : '0.5rem'};
+  transform: ${props => props.click ? 'rotate(40deg)' : 'rotate(0)'};
+}
+`;
 
 
 const Navigation = () => {
+
+  const [click, setClick] = useState(false)
 
 const scrollTo = (id) =>{
   let element = document.getElementById(id);
@@ -56,10 +121,13 @@ const scrollTo = (id) =>{
 }
 
   return (
-    <Section>
+    <Section id="navegation">
       <NavBar>
         <Logo />
-        <Menu>
+        <HamburgerMenu click={click} onClick={()=> setClick(!click)}>
+          &nbsp;
+        </HamburgerMenu>
+        <Menu click={click}>
           <MenuItem onClick={()=> scrollTo('home')}>Home</MenuItem>
           <MenuItem onClick={()=> scrollTo('about')}>About</MenuItem>
           <MenuItem onClick={()=> scrollTo('roadmap')}>Roadmap</MenuItem>
